a7dfe305a4db26049d5f740d23ff23ed963d4588
Warren Niu <warrenniu@gmail.com>
2020-09-24 16:56:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentence = sentence.split
 
 
-  sentence.each do |element|
+  sentence.collect do |element|
     element = element.reverse!
     end
     sentence.join(" ")
